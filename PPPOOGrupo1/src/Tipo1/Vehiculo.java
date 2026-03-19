61348db25401848a7a4b7a7f2810f7ea75b050ac
juisazar <juisazar@unal.edu.co>
2026-03-19 11:00:59 -0500

    version2
    Creacion clase camion y vehiculo

---
+++ PPPOOGrupo1/src/Tipo1/Vehiculo.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tipo1;
+
+/**
+ *
+ * @author Juan Diego Isaza - Juan Manuel Galeano
+ */
+public abstract class  Vehiculo {
+    String placa;
+    String modelo;
+    String marca;
+
+    public Vehiculo(String placa, String modelo, String marca) {
+        this.placa = placa;
+        this.modelo = modelo;
+        this.marca = marca;
+    }
+    
+    abstract float calcularAutonomia();
+    
+    String mostrarInformacionGeneral(){
+        return "Vehiculo{" + "placa=" + placa + ", modelo=" + modelo + ", marca=" + marca + '}';
+    }
+
+
+    
+}
