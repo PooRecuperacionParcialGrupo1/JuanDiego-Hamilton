ed6a19283a92ee0dc393aab3b626951287770ddf
juisazar <juisazar@unal.edu.co>
2026-03-22 17:28:50 -0500

    conjunto
    ...

--- PPPOOGrupo1/src/Tipo1/Camion.java
+++ PPPOOGrupo1/src/Tipo1/Camion.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Tipo1;
 
 /**
@@ -15,7 +11,7 @@ public class Camion extends Vehiculo {
     public Camion(double capacidadCargaToneladas, String placa, String modelo, String marca) {
         super(placa, modelo, marca);
         this.capacidadCargaToneladas = capacidadCargaToneladas;
-    }
+    } 
     
     
 
